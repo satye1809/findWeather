e6ebb1969eeec791d55ada2b2b8d8a3aa7ad620a
satye1809 <satyendray384@gmail.com>
2020-10-19 16:50:36 +0530

    app.js comments removed

--- src/app.js
+++ src/app.js
@@ -6,17 +6,15 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
-// Define paths for Express config
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlebars engine and views location
+
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
-
-// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -25,7 +23,6 @@ app.get('', (req, res) => {
         name: 'Satyendra Kumar'
     })
 })
-
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
